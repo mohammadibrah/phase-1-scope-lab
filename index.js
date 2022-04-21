6312eeede6f4cfb791f8dca2f181184d43cd3116
m.ibrahim <mohammad.si.tt@gmail.com>
2022-04-22 01:45:07 +0300

    Completed assignment!

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'asd'
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(best){
+    bestCustomer = best;
+}
+
+function changeLeastFavoriteCustomer(customer){
+    leastFavoriteCustomer = customer;
+}
